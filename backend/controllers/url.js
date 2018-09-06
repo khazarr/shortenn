770147a9e79c830f54acfe0bc6ba0f418aad44ee
Karol Swieca <swieca.karol@gmail.com>
2018-09-06 16:31:25 +0200

    basic refactor

--- backend/controllers/url.js
+++ backend/controllers/url.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
 const UrlModel = mongoose.model("UrlModel");
 const errorUrl = 'http://localhost/error';
+const validUrl = require("valid-url");
+const shortid = require("shortid");
 
 module.exports = {
-  getShorten: async (req, res) => {
-    console.log('wjezdzam tu')
+  getShortenUrl: async (req, res) => {
+      console.log('wjezdzam tu')
       const urlCode = req.params.shortenUrl;
       const item = await UrlModel.findOne({
         urlCode: urlCode
@@ -15,5 +17,58 @@ module.exports = {
       } else {
         return res.status(400).json('ni ma tego w bazie');
       }
+    },
+    postShortenUrl: async (req, res) => {
+      const {
+        originalUrl,
+        shortBaseUrl
+      } = req.body;
+      const urlCode = shorid.generate();
+      const updatedAt = new Date();
+      const validation = helpers.validateParameters(originalUrl, shortBaseUrl)
+      if (!validation.status) {
+        return res
+          .status(400)
+          .json({
+            msg: "Bad parameters",
+            err: validation
+          })
+      } else {
+        try {
+          const item = await UrlModel.findOne({
+            originalUrl: originalUrl
+          });
+          if (item) {
+            res.status(200).json(item)
+          } else {
+            shortUrl = shortBaseUrl + "/" + urlCode;
+            const item = new UrlModel({
+              originalUrl,
+              shortUrl,
+              urlCode,
+              updatedAt
+            });
+            await item.save();
+            res.status(200).json(item);
+          }
+        } catch (error) {
+          res.status(500).json({
+            msg: "Database problem ;(",
+            error
+          })
+        }
+      }
+    }
+}
+
+const helpers = {
+  validateParameters(url, base) {
+    const urlValid = !!validUrl.isWebUri(url)
+    const baseValid = !!validUrl.isUri(base)
+    return {
+      status: urlValid && baseValid,
+      urlValid,
+      baseValid
+    }
   }
 }
--- backend/routes/urlshorten.js
+++ backend/routes/urlshorten.js
@@ -1,48 +1,12 @@
-const mongoose = require("mongoose");
-const validUrl = require("valid-url");
-const UrlModel = mongoose.model("UrlModel");
-const shortid = require("shortid");
-const errorUrl = 'http://localhost/error';
 const UrlControler = require('../controllers/url')
 module.exports = app => {
   app.post("/", async (req, res) => {
-    const {
-      originalUrl,
-      shortBaseUrl
-    } = req.body;
-    const urlCode = shortid.generate();
-    const updatedAt = new Date();
-
-    if (!validUrl.isUri(shortBaseUrl)) {
-      return res
-        .status(401)
-        .json(
-          "Invalid Base Url"
-        );
-    } else {
-      try {
-        const item = await UrlModel.findOne({
-          originalUrl: originalUrl
-        });
-        if (item) {
-          res.status(200).json(item);
-        } else {
-          shortUrl = shortBaseUrl + "/" + urlCode;
-          const item = new UrlModel({
-            originalUrl,
-            shortUrl,
-            urlCode,
-            updatedAt
-          });
-          await item.save();
-          res.status(200).json(item);
-        }
-      } catch (err) {
-        res.status(401).json("Invalid User Id");
-      }
-    }
+    return UrlControler.postShortenUrl(req, res)
   });
+  app.get('/getAllUrls', (req, res) => {
+    return res.status(200).json('TO DO >.<')
+  })
   app.get("/:shortenUrl", async (req, res) => {
-    return UrlControler.getShorten(req,res)
+    return UrlControler.getShortenUrl(req, res)
   })
 };
